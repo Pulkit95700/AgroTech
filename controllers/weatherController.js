73e5bae66604a7957a7b8a100392d20555b9b2ce
Pulkit Gupta <pulkitgupta95700@gmail.com>
2023-03-31 07:53:54 +0530

    Update weatherController.js

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -8,7 +8,18 @@ const getWeatherData = asyncHandler(async (req, res) => {
     throw new Error("City is required");
   }
   try {
- 
+      const {data} = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat=23.2584857&lon=77.401989&appid="+ process.env.WEATHER_API_KEY)
+
+      res.status(200).json({
+        id: data.weather[0].id,
+        type: data.weather[0].main,
+        description: data.weather[0].description,
+        temp: data.main.temp,
+        feels_like: data.main.feels_like,
+        pressure: data.main.pressure,
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+      })
 
   } catch (err) {
     res.status(500);
