931f17509862cafc76c38499ae45fb3d521e2859
Pulkit Gupta <pulkitgupta95700@gmail.com>
2023-04-01 05:03:27 +0530

    Update mspController.js

--- controllers/mspController.js
+++ controllers/mspController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const axios = require("axios");
-const Msp = require("../models/mspModel");
+const Msp = require("../models/MspModel");
 
 const getMSPs = asyncHandler(async (req, res) => {
     try{
